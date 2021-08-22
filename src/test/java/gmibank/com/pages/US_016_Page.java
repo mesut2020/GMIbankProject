5278980273ebb08046be61b0b3f34827618adb0d
bahatin <knk.bahatin@gmail.com>
2021-08-22 22:58:29 +0200

    US_007_Page

---
+++ src/test/java/gmibank/com/pages/US_016_Page.java
@@ -0,0 +1,164 @@
+package gmibank.com.pages;
+
+import gmibank.com.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class US_016_Page {
+
+    public US_016_Page(){PageFactory.initElements(Driver.getDriver(),this);}
+
+    WebDriver driver= Driver.getDriver();
+
+    GmiSignInPage gmiSignInPage = new GmiSignInPage();
+
+    WebElement myElement;
+
+    @FindBy(id = "username")
+    private WebElement username;
+
+    @FindBy(id = "password")
+    private WebElement password;
+
+    @FindBy(xpath = "//button[@class='btn btn-primary']")
+    private WebElement submitButton;
+
+    @FindBy(xpath ="//span[contains(text(),'Operations')]")
+    private WebElement myOperationsDropDown;
+
+    @FindBy(linkText = "My Accounts")
+    private WebElement myAccounts;
+
+    @FindAll({@FindBy(xpath = "//table[@class='table table-striped']/tbody/tr")})
+    private List<WebElement> accounts;
+
+    @FindBy(xpath ="//a[contains(text(),'Transfer Money')]")
+    private WebElement transferMoney;
+
+    @FindBy(xpath = "//select[@name='fromAccountId']")
+    private WebElement fromAccount;
+
+    @FindBy(xpath = "//select[@name='toAccountId']")
+    private WebElement toAccount;
+
+    @FindBy(xpath = "//input[@name='balance']")
+    private WebElement balance;
+
+    @FindBy(xpath = "//input[@name='balancecent']")
+    private WebElement balanceCent;
+
+
+    @FindBy(xpath = "//textarea[@name='description']")
+    private WebElement description;
+
+    @FindBy(id = "make-transfer")
+    private WebElement makeTransfer;
+
+    @FindBy(xpath = "//div[@role='alert']")
+    private WebElement succesfullMessage;
+
+
+
+    public void goToSignInPage() {
+        Driver.iAmOnHomePage();
+
+    }
+
+
+    public void clickToWebElement(String element){
+
+        switch (element){case "submitButton": myElement=submitButton;break;
+
+       case "myOperationsDropDown": myElement=myOperationsDropDown;break;
+
+       case "myAccounts": myElement=myAccounts;break;
+
+        case "transferMoney": myElement=transferMoney;break;
+
+        case "fromAccount": myElement=fromAccount;break;
+
+        case "toAccount": myElement=toAccount;break;
+
+        case "balance": myElement=balance;break;
+
+        case "description": myElement=description;break;
+
+        case "makeTransfer": myElement=makeTransfer;break;}
+
+        Driver.waitAndClick(myElement,2);
+    }
+
+
+    public void sendKeysToWebElement(String element, String value){
+
+
+        switch (element){case "username": myElement=username;break;
+
+        case "password": myElement= password;break;
+
+        case "balance": myElement=balance;break;
+
+        case "balanceCent": myElement=balanceCent;break;
+
+        case "description": myElement=description;break;}
+
+
+myElement.clear();
+
+        Driver.waitAndSendText(myElement, value,2);
+
+    }
+
+
+
+    public boolean isAccountExist(String element){
+
+//        System.out.println(accounts.size());
+//        System.out.println(accounts.get(0).getText());
+
+        return accounts.size()>=2;
+
+    }
+
+    public String selectFromAccount(String element,String text){
+        Driver.wait(2);
+
+        Select selectFromAccount=new Select(fromAccount);
+        selectFromAccount.selectByValue(text);
+
+
+return text;
+
+    }
+    public String selectToAccount(String element,String text){
+        Driver.wait(2);
+
+        Select selectToAccount=new Select(toAccount);
+        selectToAccount.selectByValue(text);
+        // selectAccount.selectByVisibleText(text);
+
+
+        return text;
+
+    }
+
+
+    public boolean verifyText(String element, String message){
+Driver.waitForVisibility(succesfullMessage,2);
+
+        System.out.println(succesfullMessage.getText()+ ":"+ message);
+
+        if (succesfullMessage.getText().contains(message)) return true;
+
+       else return false;
+    }
+}
---
+++ src/test/java/gmibank/com/stepDefinitions/US_016_StepDefinitions.java
@@ -0,0 +1,77 @@
+package gmibank.com.stepDefinitions;
+
+import gmibank.com.pages.US_007_Page;
+import gmibank.com.pages.US_016_Page;
+import gmibank.com.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import org.junit.Assert;
+
+public class US_016_StepDefinitions {
+
+    US_016_Page us_016_page= new US_016_Page();
+
+    @When("Click on My Operations menu")
+    public void clickOnMyOperationsMenu() {
+        us_016_page.clickToWebElement("myOperationsDropDown");
+    }
+
+    @And("Click on My Account")
+    public void clickOnMyAccount() {
+us_016_page.clickToWebElement("myAccounts");
+    }
+
+    @Then("User should be able to see at least 2 Account")
+    public void userShouldBeAbleToSeeAtLeastAccount() {
+        Assert.assertTrue(us_016_page.isAccountExist("accounts"));
+    }
+
+    @And("Click on Transfer Money")
+    public void clickOnTransferMoney() {
+        us_016_page.clickToWebElement("transferMoney");
+    }
+
+    @And("Select first account as From dropdown")
+    public void selectFirstAccountAsFromDropdown() {
+       us_016_page.selectFromAccount("selectFromAccount","6687");
+    }
+
+    @And("Select first account as To dropdown")
+    public void selectFirstAccountAsToDropdown() {
+        us_016_page.selectToAccount("selectToAccount","6688");
+    }
+
+    @And("Click on the Balance and type {string} Euro {string} Cent")
+    public void clickOnTheBalanceAndType(String e,String c) {
+        us_016_page.clickToWebElement("balance");
+        us_016_page.sendKeysToWebElement("balance",e);
+        us_016_page.sendKeysToWebElement("balanceCent",c);
+    }
+
+    @And("Provide a description as {string}")
+    public void provideADescriptionAs(String text) {
+        us_016_page.clickToWebElement("description");
+        us_016_page.sendKeysToWebElement("description",text);
+    }
+
+    @And("Click on Make Transfer button")
+    public void clickOnMakeTransferButton() {
+        us_016_page.clickToWebElement("makeTransfer");
+
+    }
+
+    @Then("User sees the successfully message as {string}")
+    public void userSeesTheSuccessfullyMessageAs(String message) {
+
+        Assert.assertTrue(us_016_page.verifyText("succesfullMessage",message));
+    }
+
+@After
+    public void after(){
+    Driver.closeDriver();
+}
+
+}
