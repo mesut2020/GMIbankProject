275f2d2cc14acb97c54152e1698490fe4a9e8e92
Harun <67702854+hrndgc@users.noreply.github.com>
2021-08-23 00:05:24 +0300

    gitignore change

--- src/test/java/gmibank/com/pages/US_017_Page.java
+++ src/test/java/gmibank/com/pages/US_017_Page.java
@@ -20,12 +20,18 @@ public class US_017_Page {
         public WebElement passwordBox ;
         @FindBy(xpath = "//button[@type='submit']")
         public WebElement signInButton ;
+
         @FindBy(css= " #admin-menu")
         public WebElement administrationButton ;
         @FindBy(partialLinkText = "User management")
         public WebElement userManagementButton ;
+
         @FindBy(partialLinkText= "Create a new user")
         public WebElement createANewUserButton ;
+
+        @FindBy (id = "authorities")
+        public WebElement profilesOptions;
+
         @FindBy(xpath = "//input[@name='login']")
         public WebElement loginNameBox ;
         @FindBy(xpath = "//input[@name='firstName']")
@@ -34,8 +40,10 @@ public class US_017_Page {
         public WebElement surnameBox ;
         @FindBy(xpath = "//input[@name='email']")
         public WebElement emailBox ;
+
         @FindBy(id= "langKey")
         public WebElement languageDropDown ;
+
         @FindBy(xpath = "//*[@id=\"authorities\"]/option[1]")
         public WebElement roleAdminOption ;
         @FindBy(xpath = "//*[@id=\"authorities\"]/option[2]']")
@@ -46,10 +54,12 @@ public class US_017_Page {
         public WebElement roleManagerOption ;
         @FindBy(xpath = "//*[@id=\"authorities\"]/option[5]")
         public WebElement roleCustomerOption ;
+
         //@FindBy(xpath = "//button[@type='submit']")
         //*[@id="app-view-container"]/div[1]/div/div/div/div[2]/div/form/button
-        @FindBy(xpath = "//*[@id=\"app-view-container\"]/div[1]/div/div/div/div[2]/div/form/button")
+        @FindBy(xpath = "//button[@type='submit']")
         public WebElement saveButton ;
+
         @FindBy(xpath = "//*[@id=\"app-view-container\"]/div[1]/div/div/div/div[2]/div[1]/div/span")
         public WebElement lastPageButton ;
         @FindBy(xpath = "//select[@id='authorities']")
--- src/test/java/gmibank/com/stepDefinitions/US_017_StepDefinitions.java
+++ src/test/java/gmibank/com/stepDefinitions/US_017_StepDefinitions.java
@@ -1,4 +1,67 @@
 package gmibank.com.stepDefinitions;
 
+import com.github.javafaker.Faker;
+import gmibank.com.pages.US_004_Page;
+import gmibank.com.pages.US_017_Page;
+import gmibank.com.utilities.Driver;
+import io.cucumber.java.en.*;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.Select;
+
 public class US_017_StepDefinitions {
+
+    US_017_Page us_017_page= new US_017_Page();
+
+    @And("user click on Administration > User Mangement  on Header")
+    public void userClickOnAdministrationUserMangementOnHeader() {
+        us_017_page.administrationButton.click();
+        us_017_page.userManagementButton.click();
+    }
+
+    @And("user click on Create a new user button on Management page")
+    public void userClickOnCreateANewUserButtonOnManagementPage() {
+        us_017_page.createANewUserButton.click();
+    }
+
+    @And("user fill in Login, First name, Last name and email fields")
+    public void user_fill_in_login_first_name_last_name_and_email_fields() {
+        Faker faker = new Faker();
+        String loginName = faker.name().username();
+        us_017_page.loginNameBox.sendKeys(loginName);
+        us_017_page.nameBox.sendKeys(faker.name().firstName());
+        us_017_page.surnameBox.sendKeys(faker.name().lastName());
+        us_017_page.emailBox.sendKeys(faker.internet().emailAddress());
+
+    }
+
+    @And("user select {string} on profiles section")
+    public void user_select_on_profiles_section(String role) {
+        Select select = new Select(us_017_page.profilesOptions);
+        switch (role) {
+            case "ROLE_USER":
+                select.selectByValue("ROLE_USER");
+                break;
+            case "ROLE_EMPLOYEE":
+                select.selectByValue("ROLE_EMPLOYEE");
+                break;
+            case "ROLE_MANAGER":
+                select.selectByValue("ROLE_MANAGER");
+                break;
+            case "ROLE_ADMIN":
+                select.selectByValue("ROLE_ADMIN");
+                break;
+        }
+    }
+
+    @And("user click on Save button")
+    public void user_click_on_save_button() {
+        us_017_page.saveButton.click();
+
+    }
+
+    @Then("user verifies Success message")
+    public void userVerifiesSuccessMessage() throws InterruptedException {
+        Thread.sleep(3000);
+        Assert.assertTrue(us_017_page.succesText.getText().contains("is created with identifier"));
+    }
 }
